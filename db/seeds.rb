af268a1f52470aa981e4f212f1fc68c7609642f0
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2019-10-23 19:13:18 -0300

    exc. finshed, style remaining

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '11 44 55 77 88'
+  },
+
+   {
+    name:         'Tipo 7',
+    address:      'niceto vega',
+    category:     'japanese',
+    phone_number: '11 44 55 66 88'
+  },
+
+  {
+    name:         'Bellagamba',
+    address:      'Armenia',
+    category:     'chinese',
+    phone_number: '11 44 45 77 88'
+  },
+
+   {
+    name:         'La hormiga',
+    address:      'plaza serrano',
+    category:     'italian',
+    phone_number: '11 44 55 88 88'
+  },
+
+   {
+    name:         'Kansas',
+    address:      'Palermo',
+    category:     'french',
+    phone_number: '11 22 55 77 88'
+  },
+
+   {
+    name:         'La parollacia',
+    address:      'libertador 2245',
+    category:     'italian',
+    phone_number: '22 44 55 77 88'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
